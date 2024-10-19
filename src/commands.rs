4aac1ae3dbfbe4ec4fd737b668b604e54863ba63
Hrólfur <hrolfurgylfa@users.noreply.github.com>
2024-10-19 21:30:24 +0000

    Added the option to add full playlists to the queue

--- src/commands.rs
+++ src/commands.rs
@@ -1,7 +1,9 @@
-use std::{process, sync::Arc, time::Duration};
+use std::{fmt::Write, process, sync::Arc, time::Duration};
 
+use serde::Deserialize;
 use songbird::{input::YoutubeDl, tracks::Track, CoreEvent, TrackEvent};
 
+use tokio::process::Command as TokioCommand;
 use tracing::{error, instrument};
 
 use crate::{
@@ -33,19 +35,94 @@ pub async fn help(
     Ok(())
 }
 
+async fn get_single_track<'a>(mut src: YoutubeDl<'static>) -> Track {
+    let mut aux_multiple = src
+        .search(Some(1))
+        .await
+        .expect("Failed to get info about song.")
+        .collect::<Vec<_>>();
+    if aux_multiple.len() == 0 {}
+    let aux = aux_multiple.swap_remove(0);
+    let title = aux.title.unwrap_or_else(|| "Unknown".to_owned());
+    let duration = aux.duration.unwrap_or(Duration::ZERO);
+
+    Track::new_with_data(
+        src.into(),
+        Arc::new(TrackData {
+            title,
+            url: aux.source_url,
+            duration,
+        }),
+    )
+}
+
+#[derive(Deserialize)]
+struct YtDlpFlatPlaylistOutput {
+    title: String,
+    url: String,
+    duration: f32,
+}
+
+async fn get_multiple_tracks(client: reqwest::Client, url: &str) -> Vec<Track> {
+    let output = TokioCommand::new("yt-dlp")
+        .arg("-j")
+        .arg(&url)
+        .arg("--flat-playlist")
+        .output()
+        .await
+        .expect("Could not find yt-dlp on path");
+    if !output.status.success() {
+        panic!(
+            "yt-dlp failed with non-zero status code: {}",
+            std::str::from_utf8(&output.stderr[..]).unwrap_or("<no error message>")
+        );
+    }
+    let out = output
+        .stdout
+        .split(|&b| b == b'\n')
+        .filter(|&x| (!x.is_empty()))
+        .take(50)
+        .map(serde_json::from_slice)
+        .collect::<Result<Vec<YtDlpFlatPlaylistOutput>, _>>()
+        .expect("Failed to parse yt-dlp playlist output.")
+        .into_iter()
+        .map(|output| {
+            Track::new_with_data(
+                YoutubeDl::new(client.clone(), output.url.clone()).into(),
+                Arc::new(TrackData {
+                    title: output.title,
+                    duration: Duration::from_secs_f32(output.duration),
+                    url: Some(output.url.clone()),
+                }),
+            )
+        })
+        .collect::<Vec<Track>>();
+
+    out
+}
+
 /// Play a song or search YouTube for a song
 #[instrument]
 #[poise::command(prefix_command, slash_command, guild_only)]
 pub async fn play(
     ctx: Context<'_>,
     #[description = "What to play"] play: String,
+    #[description = "Put the full playlist onto the queue?"]
+    #[flag]
+    playlist: bool,
 ) -> Result<(), Error> {
+    let _defer = ctx.defer_or_broadcast().await?;
+
     let Some(guild_id) = ctx.guild().map(|g| g.id) else {
         ctx.say("This command is only supported in guilds.").await?;
         return Ok(());
     };
 
-    let _defer = ctx.defer_or_broadcast().await?;
+    let songbird = get_songbird_manager(ctx).await;
+    let Some(driver_lock) = songbird.get(guild_id) else {
+        ctx.say("Not in voice channel, can't play.").await?;
+        return Ok(());
+    };
 
     // Some prepwork before gathering the data
     let do_search = !play.starts_with("http");
@@ -57,41 +134,40 @@ pub async fn play(
     };
 
     // Fetch data about the selected video
-    let mut src = if do_search {
-        YoutubeDl::new_search(http_client, play.clone())
+    let tracks = if do_search {
+        let src = YoutubeDl::new_search(http_client, play);
+        vec![get_single_track(src).await]
     } else {
-        YoutubeDl::new(http_client, play.clone())
+        if playlist {
+            get_multiple_tracks(http_client, &play).await
+        } else {
+            let src = YoutubeDl::new(http_client, play);
+            vec![get_single_track(src).await]
+        }
     };
-    let mut aux_multiple = src
-        .search(Some(1))
-        .await
-        .expect("Failed to get info about song.")
-        .collect::<Vec<_>>();
-    if aux_multiple.len() == 0 {}
-    let aux = aux_multiple.swap_remove(0);
-    let title = aux.title.unwrap_or_else(|| "Unknown".to_owned());
-    let duration = aux.duration.unwrap_or(Duration::ZERO);
 
     // Add the song to the queue
+    let mut songs_added = vec![];
     {
-        let songbird = get_songbird_manager(ctx).await;
-        let Some(driver_lock) = songbird.get(guild_id) else {
-            ctx.say("Not in voice channel, can't play.").await?;
-            return Ok(());
-        };
         let mut driver = driver_lock.lock().await;
-        let track = Track::new_with_data(
-            src.into(),
-            Arc::new(TrackData {
-                title: title.clone(),
-                url: aux.source_url,
-                duration,
-            }),
-        );
-        driver.enqueue(track).await;
-    };
+        for track in tracks {
+            let data = track.user_data.downcast_ref::<TrackData>().unwrap();
+            let preload_time = data.duration.saturating_sub(Duration::from_secs(5));
+            songs_added.push(data.title.clone());
+            driver.enqueue_with_preload(track, Some(preload_time));
+        }
+    }
 
-    ctx.say(format!("\"{}\" added to queue.", title)).await?;
+    // Make the list of songs added for discord
+    let mut songs_added_str = format!("\"{}\"", songs_added[0]);
+    if songs_added.len() > 1 {
+        for title in songs_added.iter().skip(1).take(songs_added.len() - 2) {
+            write!(songs_added_str, ", \"{}\"", title).unwrap();
+        }
+        write!(songs_added_str, " and \"{}\"", songs_added.last().unwrap()).unwrap();
+    }
+    ctx.say(format!("{} added to queue.", songs_added_str))
+        .await?;
 
     send_playlist_info(ctx, guild_id).await
 }
